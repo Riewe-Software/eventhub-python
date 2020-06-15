a75d812a31c582eff52afb974ce98e4af11e512d
fabianriewe <f.riewe@riewe-software.de>
2020-06-15 10:07:27 +0200

    refactored package import

--- tests/test_eventhub.py
+++ tests/test_eventhub.py
@@ -1,7 +1,7 @@
-from eventhub.eventhub import Eventhub
+from eventhub import Eventhub
 
 eventhub = Eventhub(base_url="http://127.0.0.1:5000",
-                    email="fabianriewe00@gmail.com",
+                    email="foo@bar.com",
                     password="123456",
                     organization_name="AMS-Pro",
                     workspace_name="ams-dev",
