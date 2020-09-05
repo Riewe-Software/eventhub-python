ab9cbfbf3b71c56a4d65766671ef6576204eca41
fabianriewe <f.riewe@riewe-software.de>
2020-09-05 13:52:55 +0200

    fix: added new backend test url

--- tests/test_eventhub.py
+++ tests/test_eventhub.py
@@ -1,12 +1,12 @@
 from eventhub import Eventhub
 
-eventhub = Eventhub(base_url="http://127.0.0.1:5000",
+eventhub = Eventhub(base_url="https://eventhub-backend-dev.herokuapp.com/",
                     email="foo@bar.com",
                     password="123456",
                     organization_name="AMS-Pro",
                     workspace_name="ams-dev",
                     app_name="ams-pro-client-b")
 
-test_data = {"to": "1"}
+test_data = {"firstName": "1", "lastName": "", "email": ""}
 
 eventhub.validate_event("user.created", test_data)
